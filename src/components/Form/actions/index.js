adfb5d90b1feb99e2b9d848907c8fb954c8005cc
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-07-20 03:48:32 +0100

    Create an action for adding Shoe details to the database

---
+++ src/components/Form/actions/index.js
@@ -0,0 +1,16 @@
+import { ADD_SHOE } from "../actions/types";
+import apiPlaceholder from "../../../apis/apiPlaceholder";
+
+export const onPostShoes = data => async dispatch => {
+  try {
+    const response = await apiPlaceholder.post("/post", data);
+    dispatch({
+      type: ADD_SHOE,
+      payload: response.data
+    });
+  } catch (error) {
+    console.log(error);
+  } finally {
+    console.log("done");
+  }
+};
