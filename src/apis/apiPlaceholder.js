0bd07213dc21cd9fae5d5a14f32ca368d818a00c
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-07-25 08:23:28 +0100

    Change the baseUrl for the api

--- src/apis/apiPlaceholder.js
+++ src/apis/apiPlaceholder.js
@@ -1,4 +1,5 @@
 import axios from "axios";
 export default axios.create({
-  baseURL: "https://blooming-retreat-36054.herokuapp.com/"
+  baseURL: `https://blooming-retreat-36054.herokuapp.com/api/v1`
+  // baseURL: `https://blooming-retreat-36054.herokuapp.com/api/v1`
 });
