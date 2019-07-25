3b63cb4ce358b6afe98d2f754e304501ec6a32a3
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-07-25 15:20:26 +0100

    Remove zombie codes and add a callback function in onGetShoe action

--- src/components/Details/actions/index.js
+++ src/components/Details/actions/index.js
@@ -1,13 +1,10 @@
 import { GET_SHOES, GET_SHOE } from "./types";
 import apiPlaceholder from "../../../apis/apiPlaceholder";
-// const { getShoes } = require("../../API/index");
 
 //Action creator to get all shoes
 export const onGetShoes = () => async dispatch => {
   try {
     const shoeDetails = await apiPlaceholder.get("/products");
-    console.log(shoeDetails);
-    // const shoeDetails = await getShoes();
     return dispatch({
       type: GET_SHOES,
       payload: shoeDetails.data
@@ -20,11 +17,12 @@ export const onGetShoes = () => async dispatch => {
 };
 
 //Action creator to get a single shoe.
-
-export const onGetShoe = id => async dispatch => {
+export const onGetShoe = (id, cb) => async dispatch => {
   try {
     const response = await apiPlaceholder.get(`/products/${id}`);
-    console.log(response);
+    if (cb) {
+      cb();
+    }
     return dispatch({
       type: GET_SHOE,
       payload: response.data
